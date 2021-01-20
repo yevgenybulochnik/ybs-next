52eb40223f01606c642530fdf644ca8a1d283d6d
Yevgeny Bulochnik <yevgeny.bulochnik@gmail.com>
2021-01-19 16:38:28 -0800

    Layout component refactor

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Nav from './nav'
 
+import { Button } from '@blueprintjs/core'
+
 import styles from './layout.module.scss'
 
 
@@ -8,23 +10,35 @@ export default function Layout({
   children,
 }) {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
       </Head>
-      <header>
-        <Nav
-          branding='Yevgeny Bulochnik'
-          config={[
-            {to: '/', name: 'Home', icon: 'home'},
-            {to: '/about', name: 'About', icon: 'person'},
-            {to: '/posts/programming', name: 'Programming', icon: 'applications'}
-          ]}
-        />
-      </header>
-      <div className={styles['content-spacer']}>
+      <Nav
+        branding='Yevgeny Bulochnik'
+        config={[
+          {to: '/', name: 'Home', icon: 'home'},
+          {to: '/about', name: 'About', icon: 'person'},
+          {to: '/posts/programming', name: 'Programming', icon: 'applications'}
+        ]}
+      />
+      <div className={styles.content}>
         {children}
       </div>
-      <footer>{'Some footer'}</footer>
+      <footer>
+        <div className={styles.footerTray}>
+          <Button minimal>
+            <a href="https://github.com/yevgenybulochnik">Github</a>
+          </Button>
+          <Button minimal>
+            <a href="https://linkedin.com/in/yevgeny-eugene-bulochnik-b429a6155">
+              LinkedIn
+            </a>
+          </Button>
+          <Button minimal>
+            <a href="/contact">Contact</a>
+          </Button>
+        </div>
+      </footer>
     </div>
   )
 }
